5a71e8576967db09915fb8ef9383e2c8e3b3d9fc
Stefan Bachhofner <stefan.bachhofner@wu.ac.at>
2020-04-08 21:15:34 +0200

    added push outbound oracle skeleton.

---
+++ python/push_outbound_oracle.py
@@ -0,0 +1,29 @@
+ '''
+    This file implements the push out-bound oracles described in
+
+    Mühlberger, R. (2019). Integration of the real world to the blockchain via in-bound and outbound oracles (Unpublished Master thesis).
+    Department of Information Systems and Operations, Vienna University of Economics and Business, Vienna, Austria.
+    
+    for evalution purposes.
+
+    A push out-bound oracle is a software artifact that retrieves data from the blockchain (out-bound) triggered
+    by an on-chain event (push).
+
+    Author: Stefan Bachhofner
+'''
+
+from utils import Oracle
+
+
+class PushOutboundOracle(Oracle):
+
+    def __init__(self):
+        super(self, PushOutboundOracle).__init__()
+
+ 
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()
