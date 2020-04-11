77aa7aa3a85b35e42f5138823f1cdf303fa409d1
Stefan Bachhofner <stefan.bachhofner@wu.ac.at>
2020-04-11 17:15:38 +0200

    Fixed IndentationError: unexpected indent.

--- oracles/push_outbound_oracle.py
+++ oracles/push_outbound_oracle.py
@@ -1,9 +1,9 @@
- '''
+'''
     This file implements the push out-bound oracles described in
 
     Mühlberger, R. (2019). Integration of the real world to the blockchain via in-bound and outbound oracles (Unpublished Master thesis).
     Department of Information Systems and Operations, Vienna University of Economics and Business, Vienna, Austria.
-    
+
     for evalution purposes.
 
     A push out-bound oracle is a software artifact that retrieves data from the blockchain (out-bound) triggered
@@ -20,7 +20,7 @@ class PushOutboundOracle(_EventListeningOracle):
     def __init__(self):
         super(self, PushOutboundOracle).__init__()
 
- 
+
 def main():
     return 0
 
