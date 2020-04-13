0d6b5e33d9330eef346f9f48ad56fe53cbed4224
Stefan Bachhofner <stefan.bachhofner@wu.ac.at>
2020-04-13 21:14:45 +0200

    Implemented pull outbound oracle.

--- oracles/pull_outbound_oracle.py
+++ oracles/pull_outbound_oracle.py
@@ -12,7 +12,8 @@
     Author: Stefan Bachhofner
 '''
 
-from .utils import _Oracle
+from utils import _Oracle
+import config
 
 
 class PullOutboundOracle(_Oracle):
@@ -21,12 +22,22 @@ class PullOutboundOracle(_Oracle):
         super(PullOutboundOracle, self).__init__(*args, **kwargs)
 
     def retrieve_state_from_transaction_hash(self, transaction_hash):
-        transaction = self._web_socket.eth.getTransaction(transaction_hash)
+        transaction = self.web_socket.eth.getTransaction(transaction_hash)
         retrieved_state = self._smart_contract.decode_function_input(transaction.input)
         return retrieved_state
 
 
 def main():
+    pull_outbound_oracle = PullOutboundOracle(
+        public_address=config.PUBLIC_ADDRESS, private_address=config.PRIVATE_ADDRESS,
+        smart_contract_address=config.ARRIVAL_SMART_CONTRACT_ADDRESS,
+        web_socket=config.WEB_SOCKET,
+        abi=config.ARRIVAL_ABI)
+
+    retrieved_state = pull_outbound_oracle.retrieve_state_from_transaction_hash(
+        transaction_hash="0xf15753a9ef3d83e6d9974a936795039605449cdf2530545c8b339deaa6a7641f")
+
+    print(retrieved_state)
     return 0
 
 
