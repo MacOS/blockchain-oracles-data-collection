e8b7eb33510e11a39130a7ce362268052cd12d8d
Stefan Bachhofner <stefan.bachhofner@wu.ac.at>
2020-04-17 21:15:11 +0200

    Changed name from back_up_mongo to back_up_mongo_to_archive to better reflect what it is doing.

--- oracles/backUp.py
+++ oracles/backUp.py
@@ -8,14 +8,14 @@ def make_daily_mongo_back_up():
     r""" Makes a daily back up of the whole MongoDB at 3 AM.
     """
     scheduler = BlockingScheduler()
-    scheduler.add_job(back_up_mongo, 'cron', hour=3, minute=0)
+    scheduler.add_job(back_up_mongo_to_archive, 'cron', hour=3, minute=0)
     scheduler.start()
 
 
-def back_up_mongo(
+def back_up_mongo_to_archive(
         file_name=f'{datetime.datetime.now().date():%Y_%m_%d}_mongo_backUp',
         file_path='/home/wucloud/Dev/BackUp/'):
-    r""" Makes a complete backup from MongoDB using the mongodump command line
+    r""" Makes a complete backup from MongoDB to an archive using the mongodump command line
     program.
     
     Args:
