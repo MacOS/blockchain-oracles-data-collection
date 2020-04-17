86b6116cc515673e167592dfa23b99516cc61b45
Stefan Bachhofner <stefan.bachhofner@wu.ac.at>
2020-04-17 17:37:57 +0200

    Added backUp script.

---
+++ oracles/backUp.py
@@ -0,0 +1,33 @@
+import os
+import datetime
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+
+def make_daily_mongo_back_up():
+    r""" Makes a daily back up of the whole MongoDB at 3 AM.
+    """
+    scheduler = BlockingScheduler()
+    scheduler.add_job(back_up_mongo, 'cron', hour=3, minute=0)
+    scheduler.start()
+
+
+def back_up_mongo(
+        file_name=f'{datetime.datetime.now().date():%Y_%m_%d}_mongo_backUp',
+        file_path='/home/wucloud'):
+    r""" Makes a complete backup from MongoDB using the mongodump command line
+    program.
+    
+    Args:
+        file_name (str, optional): 
+        file_path (str, optional):
+    """
+    os.system(f'mongodump --archive="{file_path}{file_name}"')
+
+
+def main():
+    make_daily_mongo_back_up()
+
+
+if __name__ == "__main__":
+    main()
