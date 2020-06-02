1d7bea1e33ba0beede04faa74cc5b5080d6d0202
Stefan Bachhofner <stefan.bachhofner@wu.ac.at>
2020-06-02 20:41:39 +0200

    Updated comments.

--- solidity/arrival.sol
+++ solidity/arrival.sol
@@ -1,20 +1,36 @@
+/**
+    This script implements the on-chain component of the push in-bound oracle as described in Mühlberger for evalution purposes.
+
+    A push inbound oracle is a software artifact that writes data to the blockchain (in-bound) triggered
+    by an off-chain event (pull).
+
+    It is used to evalute the push inbound oracle, push outbound oracle, and the pull outbound oracle.
+
+    Author: Roman Mühlberger, Stefan Bachhofner
+
+    Mühlberger, R. (2019). Integration of the real world to the blockchain via in-bound and outbound oracles (Unpublished Master thesis).
+    Department of Information Systems and Operations, Vienna University of Economics and Business, Vienna, Austria.
+
+    Mühlberger, R., Di Ciccio, C., Castello Ferrer, E., Bachhofner, S., and Weber, I. (2020) Foundational
+    Oracle Patterns. Business Process Management: Blockchain Forum.
+**/
+
+
+
 pragma solidity ^0.4.21;
 
 
 contract Arrival {
-   //Contract for QR code scans
 
    string order;
    string location;
    uint timestamp;
 
-
-   //event with details scanned by the QR Code web application
    event Arrival(
       string order,
       string location,
       uint timestamp
-    );
+   );
 
 
    //function to emit the information from the QR code
--- solidity/customer.sol
+++ solidity/customer.sol
@@ -1,12 +1,10 @@
 /**
     This script implements the on-chain component of the pull in-bound oracle as described in Mühlberger for evalution purposes.
 
-    This code is responsible for emitting 
-
     A pull in-bound oracle is a software artifact that writes data to the blockchain (in-bound) triggered
     by an on-chain event (pull).
 
-    Author: Stefan Bachhofner
+    Author: Roman Mühlberger, Stefan Bachhofner
 
     Mühlberger, R. (2019). Integration of the real world to the blockchain via in-bound and outbound oracles (Unpublished Master thesis).
     Department of Information Systems and Operations, Vienna University of Economics and Business, Vienna, Austria.
@@ -46,11 +44,14 @@ contract Customer {
 
 
     function verifyCustomer(string _firstName, string _lastName, uint _taxID, string _email, string _product, uint _quantity, string _details) public payable{
-        emit VerifyCustomer(_firstName, _lastName, _taxID, _email, _product, _quantity, _details);
+        // Reqeust to verify customer liquidity. This here is, of course, simulated and has to be triggered
+	// somehow. We use a push inbound oracle for that purpose.
+	emit VerifyCustomer(_firstName, _lastName, _taxID, _email, _product, _quantity, _details);
     }
 
     function statusCustomerCredibility(bool _isVerified, uint _orderID, uint _errorCode) public payable {
-        if(_isVerified) {
+        // Receives the inbound state.
+	if(_isVerified) {
             emit Order(_orderID);
         } else {
             emit Error(_orderID, _errorCode);
